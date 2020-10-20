cb8489463805bc443314f08f316eeaa538febc09
Nathaly <developers>
2020-10-19 21:04:03 -0500

    Agregar archivos de proyecto.

---
+++ Semana3NSanchez/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana3NSanchez
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+            
+            //Abrir vista dos
+            await Navigation.PushAsync(new Vista_dos(user,pass));
+        }
+    }
+}
